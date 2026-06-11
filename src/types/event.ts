55c308f4d47c35e528d683f2f31955282e44ec83
Dany <c@fpc.de>
2026-06-11 15:49:42 +0200

    stand bevor implementierung ionos

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,16 +1,10 @@
 export interface EventData {
   title: string;
   date: string;
-
   createdAt: string;
-
   storageFolder: string;
-
   uploadUntil: string;
-
   deleteAfter: string;
-
   fileCount?: number;
-
   totalSizeMB?: number;
 }
